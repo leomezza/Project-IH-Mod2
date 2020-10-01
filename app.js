34cee0ccc060e8bc739894f538578eed7573f028
Leonardo Mezzanotti <leomezza@gmail.com>
2020-10-01 20:16:44 -0300

    Leo prot, Cy open

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const hbs = require('hbs');
 const mongoose = require('mongoose');
@@ -7,20 +9,20 @@ const connectMongo = require('connect-mongo');
 
 const MongoStore = connectMongo(session);
 
-const appRoutes = require('./routes/appRoutes');
-const authRoutes = require('./routes/authRoutes');
+const index = require('./routes/index.routes');
+const prot = require('./routes/prot.routes');
 
 const app = express();
 
-mongoose.connect('mongodb://localhost/shorty-database', { useUnifiedTopology: true, useNewUrlParser: true })
-  .then(() => console.log('Connected with MongoDB'))
-  .catch(error => {
-    console.log(error);
-    throw new Error('An error occurred while trying to connect with MongoDB');
-  });
+require('./configs/db.config');
+
+// Middleware Setup
+app.use(logger('dev')); //check usefulness
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(session({
-  secret: '98asdioahsd98ya8h897hiu11dasd1564651321jdiojsd89u',
+  secret: 'hjdfgdfoi17208hjsodfn7y0876234hkjhikfhusdf',
   saveUninitialized: false,
   resave: true,
   rolling: true,
@@ -31,13 +33,15 @@ app.use(session({
   }),
 }));
 
+// Express View engine setup
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 hbs.registerPartials(__dirname + '/views/partials');
-app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/', authRoutes);
-app.use('/', appRoutes);
+app.use('/', index);
+app.use('/', prot);
 
-app.listen(3000, () => console.log('App running on Port 3000'));
+server.listen(process.env.PORT, () => {
+  console.log(`Listening on http://localhost:${process.env.PORT}`);
+});
